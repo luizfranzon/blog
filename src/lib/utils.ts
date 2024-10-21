1526175fcb32a903f59b68877bddb94e1ffd1d03
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-10-21 15:20:30 -0300

    add: translations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,6 +16,6 @@ export function formatDate(date: Date) {
 export function readingTime(html: string) {
   const textOnly = html.replace(/<[^>]+>/g, "");
   const wordCount = textOnly.split(/\s+/).length;
-  const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
-  return `${readingTimeMinutes} minutos de leitura.`;
+  const readingTimeMinutes = wordCount / 200 + 1;
+  return `${readingTimeMinutes.toFixed()} ${readingTimeMinutes >= 2 ? "minutos" : "minuto"} de leitura.`;
 }
